f8a0bc0f5d74e0eb3151923eb36a806c20d29088
oncet <camilorivera86@gmail.com>
2021-01-03 01:49:31 -0300

    use next link

--- components/button.js
+++ components/button.js
@@ -1,10 +1,13 @@
+import Link from 'next/link'
+
 export default function Button({ children, href, className }) {
   return (
-    <a
-      className={`inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${className}`}
-      href={href}
-    >
-      {children}
-    </a>
+    <Link href={href}>
+      <a
+        className={`inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${className}`}
+      >
+        {children}
+      </a>
+    </Link>
   )
 }
--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function Courses() {
-  return <div><a href="/courses/hello-world">Cursos!</a></div>
+  return <Link href="/courses/hello-world"><a>Cursos!</a></Link>
 }
