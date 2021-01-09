756b5ac80ea14ed90f6ccc959e46a2723bfe76d2
oncet <camilorivera86@gmail.com>
2021-01-09 02:29:47 -0300

    only animate partial layout

--- pages/_app.js
+++ pages/_app.js
@@ -5,27 +5,28 @@ import "../styles/global.css";
 
 function MyApp({ Component, pageProps, router }) {
   return (
-    <motion.div
-      key={router.route}
-      initial="pageInitial"
-      animate="pageAnimate"
-      variants={{
-        pageInitial: {
-          opacity: 0,
-        },
-        pageAnimate: {
-          opacity: 1,
-        },
-      }}
-      className="container max-w-screen-md mx-auto border rounded shadow p-4 mt-4"
-    >
+    <div className="container max-w-screen-md mx-auto border rounded shadow p-4 mt-4">
       <p>
-        <Link href="..">
+        <Link href="/">
           <a>Home</a>
         </Link>
       </p>
-      <Component {...pageProps} />
-    </motion.div>
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </div>
   );
 }
 
