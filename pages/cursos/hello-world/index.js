8a2abe384621d97aaf5cdbe7793bb5e57f6976c6
oncet <camilorivera86@gmail.com>
2021-01-09 02:30:05 -0300

    add animations to code run

--- pages/cursos/hello-world/index.js
+++ pages/cursos/hello-world/index.js
@@ -1,21 +1,30 @@
-import { useState } from 'react'
-import Head from 'next/head'
-import CodeEditor from '../../../components/code-editor'
-import Button from '../../../components/button'
+import { useState } from "react";
+import Head from "next/head";
+import { motion } from "framer-motion";
+import CodeEditor from "../../../components/code-editor";
+import Button from "../../../components/button";
+import Spinner from "../../../components/spinner";
 
 export default function HelloWorld() {
-  const [codeExecuted, setCodeExecuted] = useState(false)
-  const [code, setCode] = useState(`const foo = 'bar'; return foo;`)
-  const [codeOutput, setCodeOutput] = useState()
+  const [isCodeBeingExecuted, setIsCodeBeingExecuted] = useState(false);
+  const [codeExecuted, setCodeExecuted] = useState(false);
+  const [code, setCode] = useState(`const foo = 'bar'; return foo;`);
+  const [codeOutput, setCodeOutput] = useState();
   const handleOnClick = () => {
-    setCodeExecuted(true)
-    try {
-      const result = Function(`"use strict"; ${code}`)(); // TODO Move out
-      setCodeOutput(result)
-    } catch(error) {
-      setCodeOutput(`${error.name} ${error.message}`)
-    }
-  }
+    setIsCodeBeingExecuted(true);
+    setTimeout(() => {
+      setCodeExecuted(true);
+      setCodeOutput(undefined);
+      try {
+        const result = Function(`"use strict"; ${code}`)(); // TODO Move out
+        setIsCodeBeingExecuted(false);
+        setCodeOutput(result);
+      } catch (error) {
+        setIsCodeBeingExecuted(false);
+        setCodeOutput(`${error.name} ${error.message}`);
+      }
+    }, 350);
+  };
   return (
     <>
       <Head>
@@ -24,22 +33,53 @@ export default function HelloWorld() {
       <h1>Hello world!</h1>
       <p>Some description...</p>
       <CodeEditor
-        language='javascript'
-        height='80px'
+        language="javascript"
+        height="80px"
         onChange={(newCode) => setCode(newCode)}
         value={code}
       />
-      <Button onClick={handleOnClick}>Probar código</Button>
+      <div className="mb-2">
+        <Button className="inline-block" onClick={handleOnClick}>
+          Probar código
+        </Button>
+        {isCodeBeingExecuted && (
+          <motion.div
+            initial="pageInitial"
+            animate="pageAnimate"
+            variants={{
+              pageInitial: {
+                opacity: 0,
+              },
+              pageAnimate: {
+                opacity: 1,
+              },
+            }}
+            className="ml-2 inline-block "
+          >
+            <Spinner />
+          </motion.div>
+        )}
+      </div>
       {codeExecuted && (
-        <>
+        <motion.div
+          initial="pageInitial"
+          animate="pageAnimate"
+          variants={{
+            pageInitial: {
+              opacity: 0,
+            },
+            pageAnimate: {
+              opacity: 1,
+            },
+          }}
+          key={!!codeOutput}
+        >
           <h2>Resultados</h2>
           <output className="block mt-4 ml-2">
-            <pre>
-              {codeOutput ? codeOutput : typeof codeOutput}
-            </pre>
+            <pre>{codeOutput ? codeOutput : typeof codeOutput}</pre>
           </output>
-        </>
+        </motion.div>
       )}
     </>
-  )
+  );
 }
