5fcbaf858dbe69783505a5996b37d28144445918
oncet <camilorivera86@gmail.com>
2021-01-10 02:14:05 -0300

    add page title

--- pages/cursos/index.js
+++ pages/cursos/index.js
@@ -1,12 +1,20 @@
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Courses() {
   return (
     <>
+      <Head>
+        <title>Cursos</title>
+      </Head>
       <h1>Cursos</h1>
       <ul>
-        <li><Link href="/cursos/hello-world"><a>Hello World</a></Link></li>
+        <li>
+          <Link href="/cursos/hello-world">
+            <a>Hello World</a>
+          </Link>
+        </li>
       </ul>
     </>
-  )
+  );
 }
